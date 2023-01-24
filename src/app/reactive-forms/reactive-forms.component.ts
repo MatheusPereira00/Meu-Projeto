42a1f892836eb7a8e182290286b56c5f85af6b5a
MatheusPereira00 <matheuspereira@ibm.com>
2023-01-24 15:48:11 -0300

    att

--- src/app/reactive-forms/reactive-forms.component.ts
+++ src/app/reactive-forms/reactive-forms.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-reactive-forms',
   templateUrl: './reactive-forms.component.html',
   styleUrls: ['./reactive-forms.component.css']
 })
-export class ReactiveFormsComponent {
+export class ReactiveFormsComponent  implements OnInit{
+
+
+  public cadastroForm: FormGroup = this.formBuilder.group({
+    firstName: [''],
+    lastName: ['']
+  });
+  
+  constructor (private formBuilder: FormBuilder){}
+
+  ngOnInit(): void {}
+
+  public submitForm(){
+    console.log(this.cadastroForm.value);
+    console.log(this.cadastroForm.value);
+  }
+
+
+
+
+
+  
+
 
 
 
